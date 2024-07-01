a2d1929c6b7c07e3af9ce650decba99d20a06e27
Andrii Hlybchenko <hlybchenko.andrii@gmail.com>
2024-07-01 15:05:30 +0300

    Initial Commit

---
+++ OneShot/OneShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneShotApp.swift
+//  OneShot
+//
+//  Created by Andrii Hlybchenko on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct OneShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
